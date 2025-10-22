6753775af58dde373f69a8960de2d475e68317f3
Joel Lim <56012934+Annihilatopia@users.noreply.github.com>
2025-10-22 16:18:49 +0800

    Update index.tsx

--- src/components/common/ChainIndicator/index.tsx
+++ src/components/common/ChainIndicator/index.tsx
@@ -73,7 +73,7 @@ const ChainIndicator = ({
     >
       {showLogo && (
         <img
-          src={chainConfig.chainLogoUri || chainConfig.nativeCurrency.logoUri || undefined}
+          src={chainConfig.chainLogoUri || chainConfig.description || undefined}
           alt={`${chainConfig.chainName} Logo`}
           width={24}
           height={24}
