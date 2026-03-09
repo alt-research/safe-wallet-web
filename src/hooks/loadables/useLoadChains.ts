e70fa7cedcdb0dfbf54f29c490464966f6e22f75
Antonio Nunez <antonio@altresear.ch>
2026-03-09 20:00:06 +0800

    updated

--- src/hooks/loadables/useLoadChains.ts
+++ src/hooks/loadables/useLoadChains.ts
@@ -32,7 +32,7 @@ export const useLoadChains = (): AsyncResult<ChainInfo[]> => {
   }, [])
 
   // Pass undefined until the gateway URL is set by the custom-chains loader
-  const [data, error, loading] = useAsync<ChainInfo[]>(deploymentsReady ? getConfigs : undefined, [deploymentsReady])
+  const [data, error, loading] = useAsync<ChainInfo[]>(() => (deploymentsReady ? getConfigs() : undefined), [deploymentsReady])
 
   // Log errors
   useEffect(() => {
